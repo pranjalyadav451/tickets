c287bf7eb66c27902a8d4e64c5dd765bdae1aeb5
pranjalyadav451 <2019uec1008@mnit.ac.in>
2022-01-11 01:19:34 +0530

    Added config for skaffold and ingress-srv

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,11 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
+app.get("/", (req, res) => {
+	console.log("reached here");
+	res.send("Hello there.");
+});
+
 app.listen(3000, () => {
 	console.log("Listening on port 3000.");
 });
