0de7bc1ac8e0b088def3deceba72de1a1c827ad1
pranjalyadav451 <2019uec1008@mnit.ac.in>
2022-01-17 20:48:37 +0530

    Added NextJS client and tickets service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,31 +1,11 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
 import mongoose from "mongoose";
-
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signupRouter } from "./routes/signup";
-import { signoutRouter } from "./routes/signout";
-import { errorHandler } from "./middleware/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-
-const app = express();
-app.use(json());
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(signoutRouter);
-
-app.all("*", async (req, res, next) => {
-	throw new NotFoundError();
-});
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
+	if (!process.env.JWT_KEY) throw new Error("JWT_KEY not defined.");
+	if (!process.env.MONGO_URI) throw new Error("MONGO_URI not defined.");
 	try {
-		await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+		await mongoose.connect(process.env.MONGO_URI);
 		console.log("Connected to MongoDB");
 	} catch (err) {
 		console.error(err);
--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -18,11 +18,23 @@ interface UserModel extends mongoose.Model<any> {
 	build(attrs: UserAttrs): UserDoc;
 }
 
-const userSchema = new mongoose.Schema({
-	email: { type: String, required: true },
-	password: { type: String, required: true },
-});
-userSchema.pre("save", async function (done) {
+const userSchema = new mongoose.Schema(
+	{
+		email: { type: String, required: true },
+		password: { type: String, required: true },
+	},
+	{
+		toJSON: {
+			transform(doc, ret) {
+				ret.id = ret._id;
+				delete ret._id;
+				delete ret.password;
+				delete ret.__v;
+			},
+		},
+	},
+);
+userSchema.pre("save", async function(done) {
 	if (this.isModified("password")) {
 		const hashed = await Password.toHash(this.get("password"));
 		this.set("password", hashed);
