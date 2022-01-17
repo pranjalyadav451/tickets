3d000712f2ca1810ffd53f908ec3c5783467eb2a
pranjalyadav451 <2019uec1008@mnit.ac.in>
2022-01-17 22:58:12 +0530

    Typo

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -12,7 +12,7 @@ const start = async () => {
 
 	try {
 		await mongoose.connect(process.env.MONGO_URI);
-		console.log("Connected to MongoDb");
+		console.log("Connected to MongoDB");
 	} catch (err) {
 		console.error(err);
 	}
